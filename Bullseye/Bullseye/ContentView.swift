c00eb379f7d7488fed595c14b642f2cd7d2549a2
Simone Tissi <simone.tissi@gmail.com>
2020-03-14 22:29:37 +0100

    button hit me

---
+++ Bullseye/Bullseye/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Simone Tissi on 14/03/2020.
+//  Copyright © 2020 Simone Tissi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to my first app!")
+                .fontWeight(.semibold)
+                .foregroundColor(Color.green)
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text(/*@START_MENU_TOKEN@*/"Hit me!"/*@END_MENU_TOKEN@*/)
+                    
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
