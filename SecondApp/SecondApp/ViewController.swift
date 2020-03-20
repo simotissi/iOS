07176d3340cdf21ecde56540f14ff3f4a7744e6f
Simone Tissi <simone.tissi@gmail.com>
2020-03-20 09:52:52 +0100

    add second app project

---
+++ SecondApp/SecondApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondApp
+//
+//  Created by Simone Tissi on 20/03/2020.
+//  Copyright © 2020 Simone Tissi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
