3479520e8ec296d81472928a52fd641a2880d54e
Simone Tissi <simone.tissi@gmail.com>
2020-03-20 09:51:56 +0100

    Login screen with imgs

---
+++ FirstApp/FirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Simone Tissi on 19/03/2020.
+//  Copyright © 2020 Simone Tissi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    print("hello")  }
+  
+}
+
