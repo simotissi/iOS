9bf4e16705aa191647c4a059710bf5b354eb7a9a
Simone Tissi <simone.tissi@gmail.com>
2020-03-23 14:56:25 +0100

    First CollectionView app ended

--- FirstCollectionView/FirstCollectionView/ViewController.swift
+++ FirstCollectionView/FirstCollectionView/ViewController.swift
@@ -27,9 +27,9 @@ class ViewController: UIViewController {
   func configureLayout() -> UICollectionViewCompositionalLayout{
     let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.2), heightDimension: .fractionalHeight(1.0))
     let item = NSCollectionLayoutItem(layoutSize: itemSize)
-    item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+    item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
     
-    let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(44.0))
+    let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.2))
     let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
     
     let section = NSCollectionLayoutSection(group: group)
